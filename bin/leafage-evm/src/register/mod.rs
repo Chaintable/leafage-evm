15e6267ed928d5b3be2990b57774786178c08cac
DDDDOGE <zhangguyu6@gmail.com>
2025-01-15 11:46:47 +0800

    fix etcd register problem (#17)

--- bin/leafage-evm/src/register/mod.rs
+++ bin/leafage-evm/src/register/mod.rs
@@ -4,12 +4,14 @@ use etcd_client::{Client, PutOptions};
 use std::time::Duration;
 use tokio::sync::watch;
 use tokio::time::interval;
-use tracing::{error, info};
+use tracing::{debug, error, info};
 
 pub struct Register {
     etcd_cfg: EtcdRegisterConfig,
     etcd_client: Client,
     lease_id: i64,
+    key: String,
+    value: String,
 }
 
 impl Register {
@@ -45,7 +47,7 @@ impl Register {
         etcd_client
             .put(
                 key.clone(),
-                value,
+                value.clone(),
                 Some(PutOptions::new().with_lease(lease_id)),
             )
             .await?;
@@ -54,13 +56,16 @@ impl Register {
             etcd_cfg,
             etcd_client,
             lease_id,
+            key,
+            value,
         })
     }
 
-    pub async fn start(mut self) -> watch::Sender<()> {
+    pub async fn start(mut self) -> Result<watch::Sender<()>> {
         let (tx, mut rx) = watch::channel(());
         let keep_alive_interval = Duration::from_millis(self.etcd_cfg.keep_alive_interval_ms);
         let mut interval = interval(keep_alive_interval);
+        let (mut keeper, mut stream) = self.etcd_client.lease_keep_alive(self.lease_id).await?;
         tokio::spawn(async move {
             loop {
                 tokio::select! {
@@ -68,18 +73,45 @@ impl Register {
                         break;
                     }
                     _ = interval.tick() => {
-                        let res = self.etcd_client
-                            .lease_keep_alive(self.lease_id)
-                            .await;
+                        let res = keeper.keep_alive().await;
                         if let Err(e) = res {
                             error!(target: "register", "keep alive error: {e}");
-                            break;
+                            let lease = self.etcd_client.lease_grant(self.etcd_cfg.lease_ttl_s, None).await.unwrap();
+                            let lease_id = lease.id();
+                            self.etcd_client
+                            .put(
+                                self.key.clone(),
+                                self.value.clone(),
+                                Some(PutOptions::new().with_lease(lease_id)),
+                            )
+                            .await.unwrap();
+                            self.lease_id = lease_id;
+                            info!(target: "register", "reregister key:{}, lease_id: {} success", self.key, self.lease_id);
+                            (keeper, stream) = self.etcd_client.lease_keep_alive(self.lease_id).await.unwrap();
                         }
+                        let res = stream.message().await;
+                        if let Result::Ok(Some(rsp)) = res {
+                            debug!(target: "register", "lease {:?} keep alive, new ttl {:?}", rsp.id(), rsp.ttl());
+                        } else {
+                            let lease = self.etcd_client.lease_grant(self.etcd_cfg.lease_ttl_s, None).await.unwrap();
+                            let lease_id = lease.id();
+                            self.etcd_client
+                            .put(
+                                self.key.clone(),
+                                self.value.clone(),
+                                Some(PutOptions::new().with_lease(lease_id)),
+                            )
+                            .await.unwrap();
+                            self.lease_id = lease_id;
+                            info!(target: "register", "reregister key:{}, lease_id: {} success", self.key, self.lease_id);
+                            (keeper, stream) = self.etcd_client.lease_keep_alive(self.lease_id).await.unwrap();
+                        }
+
                     }
                 }
             }
         });
-        tx
+        Ok(tx)
     }
 }
 
@@ -90,7 +122,7 @@ pub async fn register_build(
 ) -> Result<watch::Sender<()>> {
     if let Some(etcd_cfg) = etcd_cfg {
         let register = Register::new(chain_id, etcd_cfg, is_archive).await?;
-        let register_handle = register.start().await;
+        let register_handle = register.start().await?;
         Ok(register_handle)
     } else {
         Ok(tokio::sync::watch::channel(()).0)
