fd4765937a996d21d631bf24533949483950f558
DDDDOGE <zhangguyu6@gmail.com>
2025-03-26 16:36:46 +0800

    fix interceptor (#25)

--- crates/leafage-evm-rpc/src/api_impl/interceptor.rs
+++ crates/leafage-evm-rpc/src/api_impl/interceptor.rs
@@ -15,7 +15,7 @@ use std::sync::{
     Arc,
 };
 use std::task::{Context, Poll};
-use std::time::{Duration, Instant, SystemTime};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 use tokio::time::interval;
 use tower::BoxError;
@@ -32,11 +32,15 @@ fn default_max_retries() -> u64 {
 }
 
 fn default_window() -> u64 {
-    180
+    3
 }
 
 fn default_stat_interval() -> u64 {
     1000
+}
+
+fn default_not_retry_threshold() -> f64 {
+    0.2
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -46,9 +50,11 @@ pub struct InterceptorConfig {
     #[serde(default = "default_max_retries")]
     pub max_retries: u64,
     #[serde(default = "default_window")]
-    pub window: u64,
+    pub window: u64, // 窗口大小 (单位: 分钟)
     #[serde(default = "default_stat_interval")]
     pub stat_interval: u64, // 采样间隔 (单位: ms)
+    #[serde(default = "default_not_retry_threshold")]
+    pub not_retry_threshold: f64, // 重试次数阈值
 }
 
 impl Default for InterceptorConfig {
@@ -58,56 +64,75 @@ impl Default for InterceptorConfig {
             max_retries: default_max_retries(),
             window: default_window(),
             stat_interval: default_stat_interval(),
+            not_retry_threshold: default_not_retry_threshold(),
         }
     }
 }
 
+#[derive(Debug, Clone)]
 struct RetryEntry {
-    timestamp: Instant,
-    retries: u64, // 重试次数
+    minute: u64,                  // 分钟数
+    request_counts: usize,        // 请求总数
+    retry_weighted_counts: usize, // 重试请求总数
 }
 
-// 重试次数直方图
-struct RetryHistogram {
+#[derive(Debug, Clone)]
+struct RetryRecorder {
     entries: VecDeque<RetryEntry>,
-    buckets: Vec<u64>, // 直方图桶，记录每个重试次数的出现频率
-    max_retries: u64,  // 最大跟踪的重试次数
-    window: Duration,
+    window: u64, // 窗口大小 (单位: 分钟)
 }
 
-impl RetryHistogram {
-    fn new(max_retries: u64, window: Duration) -> Self {
-        RetryHistogram {
+impl RetryRecorder {
+    fn new(window: u64) -> Self {
+        RetryRecorder {
             entries: VecDeque::new(),
-            buckets: vec![0; (max_retries + 1) as usize], // +1 因为包含0次重试
-            max_retries,
             window,
         }
     }
 
-    fn add(&mut self, retries: u64) {
-        let now = Instant::now();
-        let retries = retries.min(self.max_retries); // 限制最大重试次数
-        self.entries.push_back(RetryEntry {
-            timestamp: now,
-            retries,
-        });
-
-        self.buckets[retries as usize] += 1;
-
-        self.clear(now);
+    fn add(&mut self, now_minute: u64, retries: u64) {
+        if let Some(entry) = self.entries.back_mut() {
+            if entry.minute == now_minute {
+                entry.request_counts += 1;
+                entry.retry_weighted_counts += retries as usize;
+            } else {
+                self.entries.push_back(RetryEntry {
+                    minute: now_minute,
+                    request_counts: 1,
+                    retry_weighted_counts: retries as usize,
+                });
+            }
+        } else {
+            self.entries.push_back(RetryEntry {
+                minute: now_minute,
+                request_counts: 1,
+                retry_weighted_counts: retries as usize,
+            });
+        }
     }
 
     // 清理旧数据
-    fn clear(&mut self, now: Instant) {
+    fn clear(&mut self, now_minute: u64) {
         while let Some(entry) = self.entries.front() {
-            if now.duration_since(entry.timestamp) > self.window {
-                self.buckets[entry.retries as usize] -= 1;
+            if now_minute >= self.window + entry.minute {
                 self.entries.pop_front();
             } else {
                 break;
             }
         }
+    }
+
+    fn stat(&self) -> f64 {
+        let mut total_request_counts = 0;
+        let mut total_retry_weighted_counts = 0;
+        for entry in &self.entries {
+            total_request_counts += entry.request_counts;
+            total_retry_weighted_counts += entry.retry_weighted_counts;
+        }
+        if total_request_counts == 0 {
+            return 0.0;
+        }
+        total_retry_weighted_counts as f64 / total_request_counts as f64
     }
 }
 
@@ -160,9 +185,10 @@ impl InterceptorLayer {
     pub fn new(cfg: &InterceptorConfig) -> Self {
         let (worker, retries_sender, load_status, other_overloaded) = InterceptorWorker::new(
             cfg.max_retries,
-            Duration::from_secs(cfg.window),
+            cfg.window,
             cfg.cpu_threshold.clone(),
             cfg.stat_interval,
+            cfg.not_retry_threshold,
         );
         let layer = InterceptorLayer {
             load_status,
@@ -188,7 +214,7 @@ impl<S> Layer<S> for InterceptorLayer {
 }
 
 struct InterceptorWorker {
-    retry_histogram: RetryHistogram,
+    retry_recorder: RetryRecorder,
     load_status: Arc<AtomicU64>,
     other_overloaded: Arc<AtomicBool>,
     retries_receiver: UnboundedReceiver<u64>,
@@ -198,14 +224,16 @@ struct InterceptorWorker {
     total_mem_size: u64,
     stat_interval: u64,      // 采样间隔 (单位: ms)
     cpu_threshold: Vec<f64>, // CPU 使用率阈值, 例如 [45.0, 65.0, 85.0]
+    not_retry_threshold: f64,
 }
 
 impl InterceptorWorker {
     fn new(
         max_retries: u64,
-        window: Duration,
+        window: u64,
         cpu_threshold: Vec<f64>,
         stat_interval: u64,
+        not_retry_threshold: f64,
     ) -> (Self, UnboundedSender<u64>, Arc<AtomicU64>, Arc<AtomicBool>) {
         if cpu_threshold.len() != 3 {
             panic!("cpu_threshold must have exactly 3 values");
@@ -223,7 +251,7 @@ impl InterceptorWorker {
         let latest_stat = process.stat().unwrap();
         let total_mem_size = get_max_memory().unwrap_or(0);
         let worker = InterceptorWorker {
-            retry_histogram: RetryHistogram::new(max_retries, window),
+            retry_recorder: RetryRecorder::new(window),
             load_status: load_status.clone(),
             other_overloaded: other_overloaded.clone(),
             retries_receiver,
@@ -233,6 +261,7 @@ impl InterceptorWorker {
             total_mem_size,
             stat_interval,
             cpu_threshold,
+            not_retry_threshold,
         };
         info!(
             target = "interceptor",
@@ -245,15 +274,15 @@ impl InterceptorWorker {
         (worker, retries_sender, load_status, other_overloaded)
     }
 
-    fn check_load_status(&self, cpu_usage: f64, _mem_usage: f64) -> u64 {
+    fn check_load_status(&self, cpu_usage: f64, _mem_usage: f64) -> LoadStatus {
         if cpu_usage <= self.cpu_threshold[0] {
-            0 // NoRefused
+            LoadStatus::NoRefused
         } else if cpu_usage <= self.cpu_threshold[1] {
-            1 // LowRefused
+            LoadStatus::LowRefused
         } else if cpu_usage <= self.cpu_threshold[2] {
-            2 // MiddleRefused
+            LoadStatus::MiddleRefused
         } else {
-            3 // AllRefused
+            LoadStatus::AllRefused
         }
     }
 
@@ -268,33 +297,55 @@ impl InterceptorWorker {
         let mem_usage = stat.rss_bytes().get() as f64 / self.total_mem_size as f64 * 100.0;
         let status = self.check_load_status(cpu_usage, mem_usage);
         self.load_status
-            .store(status, std::sync::atomic::Ordering::SeqCst);
+            .store(status as u64, std::sync::atomic::Ordering::SeqCst);
+        self.latest_stat = stat;
         debug!(
             target = "interceptor",
-            "Load status updated: {status} (usage: {cpu_usage:.2}%, total_core_num: {}, mem_usage: {mem_usage:.2}%, total_mem_size: {})",
+            "Load status updated: {status:?} (usage: {cpu_usage:.2}%, total_core_num: {}, mem_usage: {mem_usage:.2}%, total_mem_size: {})",
             self.total_core_num, self.total_mem_size);
-        self.latest_stat = stat;
     }
 
-    fn set_other_overloaded(&self) {
-        self.other_overloaded
-            .store(false, std::sync::atomic::Ordering::SeqCst);
+    fn set_other_overloaded(&mut self) {
+        let stat = self.retry_recorder.stat();
+        self.other_overloaded.store(
+            stat >= self.not_retry_threshold,
+            std::sync::atomic::Ordering::SeqCst,
+        );
+        debug!(
+            target = "interceptor",
+            "Other overloaded status updated: {overloaded}, retry rate: {stat:.2}",
+            overloaded = self
+                .other_overloaded
+                .load(std::sync::atomic::Ordering::SeqCst),
+            stat = stat
+        );
     }
 
     async fn run(mut self) {
         let mut interval = interval(Duration::from_millis(self.stat_interval));
+        let mut now_minute = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            / 60;
         loop {
             tokio::select! {
                 msg = self.retries_receiver.recv() => {
                     if let Some(retries) = msg {
-                        self.retry_histogram.add(retries);
+                        self.retry_recorder.add(now_minute, retries);
                     } else {
-                        break; // 处理关闭通道的情况
+                        break;
                     }
                 }
                 _ = interval.tick() => {
                     self.set_load_status();
                     self.set_other_overloaded();
+                    now_minute = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs()
+                        / 60;
+                    self.retry_recorder.clear(now_minute);
                 }
             }
         }
@@ -331,28 +382,15 @@ pub struct Interceptor<T> {
 }
 
 impl<T> Interceptor<T> {
-    fn check(&self, load_shedding: &LoadShedding) -> (bool, bool) {
-        let (mut reject, mut retryable) = self.check_load_deadline(load_shedding);
-        if reject {
-            return (reject, retryable);
-        }
-        reject = self.check_load_status(load_shedding);
-        if reject {
-            retryable = !self.check_other_overloaded();
-        }
-        self.record_request(load_shedding, reject);
-        return (reject, retryable);
-    }
-
     // 规则1: 检查x-load-deadline
-    fn check_load_deadline(&self, load_shedding: &LoadShedding) -> (bool, bool) {
+    fn check_load_deadline(&self, load_shedding: &LoadShedding) -> bool {
         if let Some(deadline) = load_shedding.load_deadline {
             let now = SystemTime::now();
             if deadline < now {
-                return (true, false);
+                return true;
             }
         }
-        (false, false)
+        false
     }
 
     // 规则2: 执行负载评估
@@ -374,12 +412,14 @@ impl<T> Interceptor<T> {
         }
     }
 
+    // 规则3: 检查其他服务是否过载
     fn check_other_overloaded(&self) -> bool {
         self.other_overloaded
             .load(std::sync::atomic::Ordering::SeqCst)
     }
 
-    fn record_request(&self, load_shedding: &LoadShedding, _reject: bool) {
+    // 记录请求
+    fn record_request(&self, load_shedding: &LoadShedding) {
         let _ = self.retries_sender.send(load_shedding.load_retries);
     }
 }
@@ -431,7 +471,7 @@ impl From<&http::header::HeaderMap> for LoadShedding {
             .get("x-load-deadline")
             .and_then(|v| v.to_str().ok())
             .and_then(|s| u64::from_str(s).ok())
-            .map(|v| SystemTime::now() + Duration::from_secs(v));
+            .map(|v| SystemTime::UNIX_EPOCH + Duration::from_secs(v));
 
         let load_retries = headers
             .get("x-load-retries")
@@ -468,18 +508,39 @@ where
 
     fn call(&mut self, request: HttpRequest<B>) -> Self::Future {
         let load_shedding = LoadShedding::from(request.headers());
-        let (reject, retryable) = self.check(&load_shedding);
-        if reject {
-            let mut response = http::response::Builder::new()
-                .status(StatusCode::TOO_MANY_REQUESTS)
-                .body(HttpBody::empty())
+        // 请求超过终止时间
+        if self.check_load_deadline(&load_shedding) {
+            let response = http::response::Builder::new()
+                .status(StatusCode::REQUEST_TIMEOUT)
+                .body(HttpBody::from("Request expired"))
                 .unwrap();
-            if !retryable {
+            return Box::pin(async move { Ok(response) });
+        }
+        // 记录请求
+        self.record_request(&load_shedding);
+
+        // 根据负载状态决定是否拒绝请求
+        let reject = self.check_load_status(&load_shedding);
+
+        if reject {
+            // 判断其它节点的负载情况
+            let other_overloaded = self.check_other_overloaded();
+            if other_overloaded {
+                let mut response = http::response::Builder::new()
+                    .status(StatusCode::TOO_MANY_REQUESTS)
+                    .body(HttpBody::from("Service overloaded, no retry needed"))
+                    .unwrap();
                 response
                     .headers_mut()
                     .insert("x-load-not-retry", "true".parse().unwrap());
+                return Box::pin(async move { Ok(response) });
+            } else {
+                let response = http::response::Builder::new()
+                    .status(StatusCode::TOO_MANY_REQUESTS)
+                    .body(HttpBody::from("Service overloaded"))
+                    .unwrap();
+                return Box::pin(async move { Ok(response) });
             }
-            return Box::pin(async move { Ok(response) });
         }
         Box::pin(self.inner.call(request).map_err(Into::into))
     }
@@ -489,12 +550,42 @@ where
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn test_max_memory() {
+    #[test]
+    fn test_max_memory() {
         let a = get_max_memory();
         assert!(a.is_ok(), "Failed to get max memory: {:?}", a.err());
         let max_memory = a.unwrap();
         assert!(max_memory > 0, "Max memory should be greater than 0");
         dbg!("Max memory: {max_memory}");
+    }
+
+    #[test]
+    fn test_load_priority() {
+        assert_eq!(LoadPriority::from_str("high").unwrap(), LoadPriority::High);
+        assert_eq!(
+            LoadPriority::from_str("medium").unwrap(),
+            LoadPriority::Medium
+        );
+        assert_eq!(LoadPriority::from_str("low").unwrap(), LoadPriority::Low);
+        assert!(LoadPriority::from_str("invalid").is_err());
+    }
+
+    #[test]
+    fn test_retry_recorder() {
+        let mut recorder = RetryRecorder::new(3);
+        recorder.add(1, 1);
+        recorder.add(1, 2);
+        recorder.clear(1);
+        assert_eq!(recorder.stat(), 1.5);
+
+        recorder.add(2, 0);
+        assert_eq!(recorder.stat(), 1.0);
+        recorder.add(3, 3);
+        assert_eq!(recorder.stat(), 1.5);
+        recorder.add(4, 0);
+        assert_eq!(recorder.stat(), 1.2);
+        recorder.clear(4);
+        dbg!(&recorder.entries);
+        assert_eq!(recorder.stat(), 1.0);
     }
 }
