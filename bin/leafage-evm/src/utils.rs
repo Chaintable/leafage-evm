53b895ebff1cc023783224d83cf5f123bc4144f1
Zhang Guyu <zhangguyu6@gmail.com>
2025-07-23 14:07:31 +0000

    fix

--- bin/leafage-evm/src/utils.rs
+++ bin/leafage-evm/src/utils.rs
@@ -3,8 +3,15 @@ use anyhow::{Context, Result};
 use aws_sdk_s3::Client;
 use flate2::read;
 use leafage_evm_types::{Block, BlockStorageDiff, Transaction, H256};
+use lru::LruCache;
 use serde::{Deserialize, Serialize};
+use std::num::NonZeroUsize;
+use std::sync::LazyLock;
+use std::sync::RwLock;
 use std::{io::Read, str::FromStr};
+
+static S3_BLOCK_CACHE: LazyLock<RwLock<LruCache<H256, Block<Transaction>>>> =
+    LazyLock::new(|| RwLock::new(LruCache::new(NonZeroUsize::new(1024).unwrap())));
 
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct KafkaS3Config {
@@ -41,6 +48,9 @@ pub async fn s3_get_block_info(
     s3_chain_id: &str,
     block_hash: H256,
 ) -> Result<Block<Transaction>> {
+    if let Some(block) = S3_BLOCK_CACHE.read().unwrap().peek(&block_hash) {
+        return Ok(block.clone());
+    }
     let s3_key = format!("{}/{}/block", s3_chain_id, block_hash);
     let s3_obj = s3_client
         .get_object()
@@ -53,7 +63,11 @@ pub async fn s3_get_block_info(
     let mut gz = read::GzDecoder::new(&bytes[..]);
     let mut bytes = Vec::new();
     gz.read_to_end(&mut bytes)?;
-    let block = serde_json::from_slice(&bytes)?;
+    let block: Block<Transaction> = serde_json::from_slice(&bytes)?;
+    S3_BLOCK_CACHE
+        .write()
+        .unwrap()
+        .put(block_hash, block.clone());
     Ok(block)
 }
 
@@ -130,17 +144,35 @@ pub async fn s3_get_block_info_and_diff_by_number(
     let block_info = s3_get_block_info(s3_client, bucket_name, s3_chain_id, block_hash)
         .await
         .context(format!("s3 get block info failed, {block_hash}"))?;
-    let block_diff = s3_get_block_diff(
+    let parent_block_info = s3_get_block_info(
         s3_client,
         bucket_name,
         s3_chain_id,
-        block_info.header.state_root,
+        block_info.header.parent_hash,
     )
     .await
     .context(format!(
-        "s3 get block diff failed, {}",
-        block_info.header.state_root
+        "s3 get parent block info failed, {}",
+        block_info.header.parent_hash
     ))?;
+    let block_diff = if parent_block_info.header.state_root != block_info.header.state_root {
+        s3_get_block_diff(
+            s3_client,
+            bucket_name,
+            s3_chain_id,
+            block_info.header.state_root,
+        )
+        .await
+        .context(format!(
+            "s3 get block diff failed, {}",
+            block_info.header.state_root
+        ))?
+    } else {
+        let mut diff = BlockStorageDiff::default();
+        diff.hash = block_info.header.state_root;
+        diff.parent_hash = parent_block_info.header.state_root;
+        diff
+    };
     Ok((block_info, block_diff))
 }
 
