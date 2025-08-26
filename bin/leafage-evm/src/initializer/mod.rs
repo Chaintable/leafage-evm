65d54f8be827e56008d35e04c7b64e3abad2fea5
DDDDOGE <zhangguyu6@gmail.com>
2025-08-26 10:33:23 +0800

    feat: add RPC fallback support for block fetching in Kafka updater (#53)
    * feat: add RPC fallback support for block fetching in Kafka updater
    
    * fix
    
    * fix

--- bin/leafage-evm/src/initializer/mod.rs
+++ bin/leafage-evm/src/initializer/mod.rs
@@ -20,13 +20,19 @@ pub async fn initialize_check<DB: ArchiveDBProvider + Send + Sync + 'static>(
         .unwrap();
     let latest_db = StateDBWrapper(db);
     if latest_db.last_committed_block()?.is_none() {
-        if let Some(rpc_address) = rpc_url.clone() {
-            let mut initializer = HttpInitializer::new(latest_db, rpc_address)?;
-            initializer.init().await?;
-        } else if let Some(kafka_s3_config) = kafka_s3_cfg {
-            let mut initializer =
-                KafkaInitializer::new(latest_db, kafka_s3_config, genesis_number).await?;
-            initializer.init().await?;
+        match (rpc_url, kafka_s3_cfg) {
+            (Some(rpc_url), None) => {
+                let mut initializer = HttpInitializer::new(latest_db, rpc_url)?;
+                initializer.init().await?;
+            }
+            (rpc_url, Some(kafka_s3_cfg)) => {
+                let mut initializer =
+                    KafkaInitializer::new(latest_db, rpc_url, kafka_s3_cfg, genesis_number).await?;
+                initializer.init().await?;
+            }
+            (None, None) => {
+                anyhow::bail!("The database is uninitialized, please provide rpc_url or kafka_s3_cfg to initialize the database.");
+            }
         }
     }
     Ok(())
