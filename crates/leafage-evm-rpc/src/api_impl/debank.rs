788fb2ada767806eb213d6e100d76c3c1e51b634
DDDDOGE <zhangguyu6@gmail.com>
2025-12-16 10:53:17 +0800

    fix: enhance tracing configuration to include opcode filtering for SSTORE (#93)

--- crates/leafage-evm-rpc/src/api_impl/debank.rs
+++ crates/leafage-evm-rpc/src/api_impl/debank.rs
@@ -18,11 +18,12 @@ use leafage_evm_types::{
     DebankSingleSimulateResult, Header, JsonStorageKey, TransactionInfo, H256, KECCAK256_EMPTY,
     U256,
 };
+use revm::bytecode::OpCode;
 use revm::context::result::InvalidTransaction;
 use revm::context::result::{ExecutionResult, HaltReason};
 use revm::context::{TransactTo, Transaction as TransactionTrait};
 use revm::database::{CacheDB, DatabaseRef};
-use revm_inspectors::tracing::TracingInspectorConfig;
+use revm_inspectors::tracing::{OpcodeFilter, TracingInspectorConfig};
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::task::spawn_blocking;
@@ -661,8 +662,10 @@ where
                     continue;
                 }
             }
-            let trace_cfg = TracingInspectorConfig::default_parity().set_record_logs(true);
-            // let mut inspector = TracingInspector::new(trace_cfg);
+            let mut trace_cfg = TracingInspectorConfig::default_parity()
+                .set_record_logs(true)
+                .set_steps(true);
+            trace_cfg.record_opcodes_filter = Some(OpcodeFilter::new().enabled(OpCode::SSTORE));
             let tx = self.inner.create_txn_env(
                 &block_env,
                 tx,
