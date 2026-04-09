213480bf3e70050b646a5258a1be0920154679c1
0byteme <nichenqin2023@gmail.com>
2026-04-09 10:50:13 +0800

    refactor: simplify L1 overhead estimation by removing unnecessary tx clone

--- crates/leafage-evm-rpc/src/api_impl/debank.rs
+++ crates/leafage-evm-rpc/src/api_impl/debank.rs
@@ -862,9 +862,7 @@ where
             highest_gas_limit
         };
 
-        let mut overhead_tx = tx.clone();
-        overhead_tx.set_gas_limit(final_gas);
-        let l1_overhead = self.inner.estimate_l1_overhead(&block, &block_env, &memory_db, overhead_tx);
+        let l1_overhead = self.inner.estimate_l1_overhead(&block, &block_env, &memory_db, tx.clone());
 
         Ok(U256::from(final_gas.saturating_add(l1_overhead)))
     }
